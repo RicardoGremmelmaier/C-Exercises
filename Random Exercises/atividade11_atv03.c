bf5d63e2ea9840ae6dcc39fda025b3ad9f7632f9
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-02-24 00:11:16 -0300

    Create atividade11_atv03.c

---
+++ Random Exercises/atividade11_atv03.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#define LARGURA_FAIXA 6 /* Largura da faixa de tecido, em cm. */
+
+void pontoRolo1();
+void pontoRolo2();
+void moveAgulha();
+void rolaTecido();
+
+
+int main ()
+{
+    int i, j;
+
+    while(1){
+
+        for(i=LARGURA_FAIXA; i>0; i--){
+
+            for (j=0; j<=LARGURA_FAIXA; j++){
+
+                if (j<i)
+                moveAgulha();
+
+                else
+                pontoRolo2();
+
+            }
+
+        rolaTecido();
+        }
+
+        for(i=LARGURA_FAIXA; i>0; i--){
+
+            for (j=0; j<=LARGURA_FAIXA; j++){
+
+                if (j<i)
+                moveAgulha();
+
+                else
+                pontoRolo1();
+        }
+
+        rolaTecido();
+        }
+
+    }
+}
+
+void pontoRolo1(){
+    printf("v");
+}
+
+void pontoRolo2(){
+    printf("a");
+}
+
+void moveAgulha(){
+    printf(" ");
+}
+
+void rolaTecido(){
+    printf("\n");
+}
