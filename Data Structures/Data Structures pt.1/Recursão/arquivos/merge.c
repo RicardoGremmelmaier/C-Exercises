8dcad5960f6508fb644eed2d332244b8ccb26aa5
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-03-26 20:17:08 -0300

    Create merge.c

---
+++ Data Structures/Data Structures pt.1/Recursão/arquivos/merge.c
@@ -0,0 +1,58 @@
+#include "list.h"
+#include "list.c"
+
+/* */
+List* merge (List *A, List *B){
+    List* C = NULL;
+
+    if (A == NULL){
+        C=B;
+        return C;
+    }
+
+    if (B == NULL){
+        C=A;
+        return C;
+    }
+
+    if (A->data <= B->data){
+        C=A;
+        C->next = merge(A->next, B);
+    }
+    else{
+        C=B;
+        C->next = merge(A, B->next);
+    }
+    return C;
+
+}
+
+/* */
+int main () {
+    int k;
+    List *A = NULL;
+    for (k = 0; k <= 14; k += 2) {
+        A = insert_back (A, k);
+    }
+    printf("Lista = ");
+    print (A);
+    printf("\n");
+
+    List *B = NULL;
+    for (k = 1; k <= 9; k += 2) {
+        B = insert_back (B, k);
+    }
+    printf("Lista = ");
+    print (B);
+    printf("\n");
+
+    List *C = merge (A, B);
+    printf("Lista = ");
+    print (C);
+    printf("\n");
+
+    destroy (C);
+    return 0;
+}
+
+
